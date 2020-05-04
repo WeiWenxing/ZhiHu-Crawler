71b2cff2c992cca32bc8064747a943c5f8592a55
weiwenxing <weiwenxing@gmail.com>
2020-05-04 14:59:04 +0800

    [Feature] add get comments

---
+++ ZhiHuSpider/getComments.py
@@ -0,0 +1,111 @@
+# -*- coding:UTF-8 -*-
+
+import random
+import re
+import sys
+import time
+
+import requests
+import pymysql
+from pyquery import PyQuery as pq
+import pandas as pd
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+}
+
+BASE_URL = "https://www.zhihu.com"
+TOPIC_API = "/api/v4/topics/{topic_id}/feeds/timeline_activity"
+QUESTION_API = "/question/{question_id}"
+ANSWER_API = '/api/v4/questions/{qid}/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question.detail,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized;data[*].mark_infos[*].url;data[*].author.follower_count,badge[*].topics&limit=5&offset=0&platform=desktop&sort_by=default'
+
+
+def get_html(url):
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            # print(response.text.encode('gbk', 'ignore').decode('gbk'))
+            return response.text
+        else:
+            print('请求网页源代码错误, 错误状态码：', response.status_code)
+    except Exception as e:
+        print(e)
+        return None
+
+
+def get_json(i_url):
+    try:
+        response = requests.get(i_url, headers=headers, timeout=10)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print('请求网页json错误, 错误状态码：', response.status_code)
+    except Exception as e:
+        print(e)
+        time.sleep(60 + float(random.randint(1, 4000)) / 100)
+        return get_json(i_url)
+
+
+def get_question_info(q_url, df):
+    html = get_html(q_url)
+    doc = pq(html)
+    # div = doc('.NumberBoard-itemInner:last strong').attr('title')
+    question_url = q_url
+    question_title = doc('.QuestionPage meta[itemprop=name]').attr('content')
+    watch_count = doc('.NumberBoard-itemInner:last strong').attr('title')
+    answer_count = doc('.QuestionPage meta[itemprop=answerCount]').attr('content')
+    follower_count = doc('.QuestionPage meta[itemprop$=followerCount]').attr('content')
+    good = ''
+    rate = 0
+    if int(answer_count) != 0:
+        rate = int(watch_count) // int(answer_count)
+    if rate > 5000:
+        good = str(rate)
+    # print(str(question_title).encode('gbk', 'ignore').decode('gbk'))
+
+    df.loc[df.shape[0]] = {'title': question_title, 'url': question_url, 'watch': watch_count, 'answer': answer_count, 'rate': rate, 'follower': follower_count}
+    return
+
+
+def get_urls(init_url):
+    url_list = []
+    is_end = False
+    next_url = init_url
+
+    while not is_end:
+        # print(next_url)
+        json = get_json(next_url)
+        is_end = json['paging']['is_end']
+        # print(is_end)
+        print(next_url)
+        questions = json['data']
+        # print(str(questions).encode('gbk', 'ignore').decode('gbk'))
+        for question in questions:
+            item = question['target']
+            if 'question' in item:
+                question_id = question['target']['question']['id']
+                ques_url = BASE_URL + QUESTION_API.format(question_id=question_id)
+                url_list.append(ques_url)
+                print(question_id)
+        next_url = json['paging']['next']
+    return set(url_list)
+
+
+def collect_info(url_list, outfile):
+    data_frame = pd.DataFrame(columns=['title', 'url', 'watch', 'answer', 'rate', 'follower'])
+    for q_url in url_list:
+        get_question_info(q_url, data_frame)
+    result = data_frame.sort_values('rate', ascending=False)
+    result.to_csv(outfile, index=False, encoding='utf_8_sig')
+    return
+
+
+url = BASE_URL + TOPIC_API.format(topic_id=sys.argv[1])
+urls = get_urls(url)
+print(urls)
+collect_info(urls, sys.argv[2])
+
+# data_frame = pd.DataFrame(columns=['title', 'url', 'watch', 'answer'])
+# get_question_info(sys.argv[1], data_frame)
+# print(html.encode('gbk', 'ignore').decode('gbk'))
+# get_urls(html)
--- ZhiHuSpider/getKeyword.py
+++ ZhiHuSpider/getKeyword.py
@@ -12,10 +12,10 @@ from pyquery import PyQuery as pq
 import pandas as pd
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
 }
 
-BASE_URL = "https://www.zhihu.com"
+BASE_URL = "http://www.zhihu.com"
 TOPIC_API = "/api/v4/topics/{topic_id}/feeds/timeline_activity"
 QUESTION_API = "/question/{qid}"
 
@@ -24,6 +24,9 @@ ANSWER_API = "/api/v4/questions/{qid}/answers?include=data[*].is_normal,admin_cl
              "comment_permission,created_time,updated_time,review_info,relevant_info,question.detail,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp," \
              "is_labeled,is_recognized;data[].mark_infos[].url;data[].author.follower_count,badge[].topics&limit=5&offset=0&platform=desktop&sort_by=default"
 ANSWER_URL = "/answer/{aid}"
+COMMENT_API = '/api/v4/answers/{aid}/comments?include=data%5B*%5D.author%2Ccollapsed' \
+          '%2Creply_to_author%2Cdisliked%2Ccontent%2Cvoting%2Cvote_count%2Cis_parent_author%2Cis_author' \
+          '%2Calgorithm_right&order=normal&limit=20&offset=0&status=open'
 
 
 def get_html(i_url):
@@ -42,18 +45,59 @@ def get_html(i_url):
 def get_json(i_url):
     try:
         response = requests.get(i_url, headers=headers, timeout=10)
+        print(response.status_code)
         if response.status_code == 200:
             return response.json()
         else:
-            print('请求网页json错误, 错误状态码：', response.status_code)
+            print('request json error, code:', response.status_code)
     except Exception as e:
         print(e)
         time.sleep(60 + float(random.randint(1, 4000)) / 100)
         return get_json(i_url)
 
 
-def get_answer_info(qid, keyword):
-    adf = pd.DataFrame({'content': [], 'url': []})
+def is_in(words, content):
+    for word in words:
+        if word in content:
+            return True
+    return False
+
+
+def get_comment(qid, aid, keyword):
+    cdf = pd.DataFrame({'content': [], 'url': [], 'q_url': []})
+    capi = BASE_URL + COMMENT_API.format(aid=aid)
+    is_end = False
+    next_url = capi
+
+    while not is_end:
+        json = get_json(next_url)
+        if not 'paging' in json:
+            break
+        if not 'is_end' in json['paging']:
+            break
+        is_end = json['paging']['is_end']
+        # print(next_url)
+        comments = json['data']
+        i = 0
+        for comment in comments:
+            content_html = comment['content']
+            content = BeautifulSoup(content_html, 'html5lib').get_text()
+            if is_in(keyword, content):
+                aurl = BASE_URL + QUESTION_API.format(qid=qid) + ANSWER_URL.format(aid=aid)
+                qurl = re.search('&offset=\d+', next_url).group() + '&i=' + str(i)
+                cdf.loc[cdf.shape[0]] = {'content': content, 'url': aurl, 'q_url': qurl}
+                # print(aurl)
+                print(aurl)
+                print(qurl)
+            i=i+1
+            if i > 10:
+                break
+        next_url = json['paging']['next']
+    return cdf
+
+
+def get_answer(qid, keyword):
+    adf = pd.DataFrame({'content': [], 'url': [], 'q_url': []})
     aapi = BASE_URL + ANSWER_API.format(qid=qid)
     is_end = False
     next_url = aapi
@@ -64,13 +108,10 @@ def get_answer_info(qid, keyword):
         # print(next_url)
         answers = json['data']
         for answer in answers:
-            content_html = answer['content']
-            content = BeautifulSoup(content_html, 'html5lib').get_text()
-            if keyword in content:
-                aid = answer['id']
-                aurl = BASE_URL + QUESTION_API.format(qid=qid) + ANSWER_URL.format(aid=aid)
-                adf.loc[adf.shape[0]] = {'content': content, 'url': aurl}
-                print(aurl)
+            aid = answer['id']
+            cdf = get_comment(qid, aid, keyword)
+            cdf.to_csv(outfile, mode='a', header=False, index=False, encoding='utf_8_sig')
+            adf = pd.concat([adf, cdf], ignore_index=True)
         next_url = json['paging']['next']
     return adf
 
@@ -82,9 +123,10 @@ def get_qids(tid):
     next_url = t_url
 
     while not is_end:
+        print(next_url)
         json = get_json(next_url)
+
         is_end = json['paging']['is_end']
-        # print(next_url)
         questions = json['data']
         for question in questions:
             item = question['target']
@@ -95,15 +137,16 @@ def get_qids(tid):
     return set(qids)
 
 
-def collect_info(ids, word, outfile):
-    df = pd.DataFrame({'content': [], 'url': []})
-    for qid in ids:
-        ainfo = get_answer_info(qid, word)
-        df = pd.concat([df, ainfo], ignore_index=True)
+def collect_info(ids, outfile, word):
+    df = pd.DataFrame({'content': [], 'url': [], 'q_url': []})
     df.to_csv(outfile, index=False, encoding='utf_8_sig')
+    for qid in ids:
+        ainfo = get_answer(qid, word)
+        df = pd.concat([df, ainfo], ignore_index=True)
+    # df.to_csv(outfile, index=False, encoding='utf_8_sig')
     return
 
-
+outfile =  sys.argv[2]
 question_ids = get_qids(sys.argv[1])
-collect_info(question_ids, sys.argv[2], sys.argv[3])
+collect_info(question_ids, sys.argv[2], sys.argv[3:])
 
